3e96ce78b631ebf26672a444f63d05c0238f5cd1
MichaelSwain <michaeljswain@gmail.com>
2024-09-10 14:09:13 +0200

    Add nodemailer to send email notification

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const nodemailer = require("nodemailer");
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const port = process.env.PORT || 4000;
+const emailAuthPass = process.env.EMAIL_AUTH_PASS
 
 // parse application/json
 app.use(bodyParser.json())
@@ -18,15 +20,40 @@ app.post("/webhooks/optimizely", (req, res) => {
             req.body.data[0].changes[2].property === "enabled"
         ) {
             if (req.body.data[0].changes[2].after === true) {
-                console.log("experiment ENABLED!");
+                sendNotification("Optimizely notification", "A new test has been launched");
             } else {
-                console.log("experiment PAUSED!");
+                sendNotification("Optimizely notification", "A test has been paused");
             }
         }
     }
-
+    res.send("success")
 });
 
 app.listen(port, () => {
     console.log("app listening on port ", port);
 });
+
+function sendNotification(messageTitle, messageBody) {
+       var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                      user: 'michaeljswain@gmail.com',
+                      pass: emailAuthPass
+                    }
+                  });
+                  
+                  var mailOptions = {
+                    from: 'michaeljswain@gmail.com',
+                    to: 'michaeljswain@gmail.com',
+                    subject: messageTitle,
+                    text: messageBody
+                  };
+                  
+                  transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log('Email sent: ' + info.response);
+                    }
+                  });
+}
